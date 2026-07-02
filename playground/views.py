b722ae99fa93e3e2bd48b68fe65283c476345ef4
MostofaSeum <mostofa.seum@brainicontech.com>
2026-07-02 16:52:32 +0600

    feat: implement hello world view, template, and VS Code debugger configuration

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 
+
+def calculate():
+    x = 1
+    y = 2
+    return x
 # Create your views here.
 def say_hello(request):
-    return HttpResponse('Hello World')
+    x = calculate()
+    return render(request, 'hello.html')
