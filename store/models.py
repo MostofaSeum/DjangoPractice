c261ea36a1487677f4e7e4c073c71877d52c6e77
MostofaSeum <mostofa.seum@brainicontech.com>
2026-07-05 16:19:33 +0600

    feat: define initial database models for products, customers, orders, and shopping cart functionality

--- store/models.py
+++ store/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+    products = models.ForeignKey(Products, on_delete=models.CASCADE)
+
 class Products(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6,decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
@@ -36,8 +41,25 @@ class Order(models.Model):
     ]
     placed_at = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=1, choices=status_choices, default=STATUS_PENDING)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Products, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6,decimal_places=2)
+    
 
 class Adress(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
+    
