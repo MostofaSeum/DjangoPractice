3f0bd2f934850c732895fbe3451cba169332848d
MostofaSeum <mostofa.seum@brainicontech.com>
2026-07-05 15:48:44 +0600

    feat: create Products, Customer, and Order models in store app

--- store/models.py
+++ store/models.py
@@ -9,8 +9,33 @@ class Products(models.Model):
     last_update = models.DateTimeField(auto_now=True)
 
 class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+    membership_choices = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
+    ]
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=10)
     birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=1, choices=membership_choices, default=MEMBERSHIP_BRONZE)
+
+
+class Order(models.Model):
+    STATUS_PENDING = 'P'
+    STATUS_COMPLETE = 'C'
+    STATUS_FAILED = 'F'
+    status_choices = [
+        (STATUS_PENDING, 'Pending'),
+        (STATUS_COMPLETE, 'Complete'),
+        (STATUS_FAILED, 'Failed'),
+    ]
+    placed_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=1, choices=status_choices, default=STATUS_PENDING)
+
+
+    
