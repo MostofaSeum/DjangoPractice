9ce4543855f09159072ecbe1d1a30f2b199e84bd
MostofaSeum <mostofa.seum@brainicontech.com>
2026-07-05 15:01:14 +0600

    Added description field

--- store/models.py
+++ store/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 class Products(models.Model):
     title = models.CharField(max_length=255)
-    slug = 
+    description = models.TextField()
