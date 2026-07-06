cfbd7c8b00e5c5119007d8f958a74c75ea26362a
MostofaSeum <mostofa.seum@brainicontech.com>
2026-07-06 18:09:46 +0600

    feat: add hello template and view to display filtered product list

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
-def calculate():
-    x = 1
-    y = 2
-    return x
+from store.models import Product
 
 
 def say_hello(request):
-    x = calculate()
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    query_set = Product.objects.filter(unit_price__range=(20, 30))
+
+    return render(request, 'hello.html', {'name': 'Seum', 'products': list(query_set)})
