9fde1fc0bd6705e91af1e3053c6d12c413a309a8
MostofaSeum <mostofa.seum@brainicontech.com>
2026-07-07 10:15:10 +0600

    feat: add say_hello view to query OrderItem product IDs in playground app

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from store.models import Product
+from django.db.models import Q,F
+from store.models import Product,OrderItem
 
 
 def say_hello(request):
-    query_set = Product.objects.filter(unit_price__range=(20, 30))
+    #query_set = Product.objects.filter(unit_price__range=(20, 30))
+    #query_set = Product.objects.values('id','title','collection__title')
+    query_set = OrderItem.objects.values('product_id')
 
     return render(request, 'hello.html', {'name': 'Seum', 'products': list(query_set)})
