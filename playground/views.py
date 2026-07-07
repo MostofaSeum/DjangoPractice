5aa84946025ab8d7d0c8872a46e02dfd78ef01ba
MostofaSeum <mostofa.seum@brainicontech.com>
2026-07-07 11:07:36 +0600

    feat: add say_hello view to playground to query distinct products ordered by title

--- playground/views.py
+++ playground/views.py
@@ -7,6 +7,6 @@ from store.models import Product,OrderItem
 def say_hello(request):
     #query_set = Product.objects.filter(unit_price__range=(20, 30))
     #query_set = Product.objects.values('id','title','collection__title')
-    query_set = OrderItem.objects.values('product_id')
+    query_set = Product.objects.filter(id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
 
     return render(request, 'hello.html', {'name': 'Seum', 'products': list(query_set)})
