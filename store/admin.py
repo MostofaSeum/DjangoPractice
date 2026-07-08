15cba9784cfe90cf2ecd33f0f6ea36ef2b571a09
MostofaSeum <mostofa.seum@brainicontech.com>
2026-07-08 11:03:15 +0600

    feat: register Product and Collection models in the admin interface

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,11 @@
+from django.contrib.admin import register
 from django.contrib import admin
 from . import models
 
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price']
+ 
+    
 # Register your models here.
 admin.site.register(models.Collection)
-admin.site.register(models.Product)
