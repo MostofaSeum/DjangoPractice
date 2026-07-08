3bcbcd3d997acf77e0c00298a56127f03283537a
MostofaSeum <mostofa.seum@brainicontech.com>
2026-07-08 11:31:55 +0600

    feat: initialize store models and configure basic admin interface for products, customers, and collections

--- store/admin.py
+++ store/admin.py
@@ -5,7 +5,15 @@ from . import models
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'unit_price']
- 
     
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    ordering = ['first_name', 'last_name']
+    list_per_page = 10
+
+ 
 # Register your models here.
 admin.site.register(models.Collection)
