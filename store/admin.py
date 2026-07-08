ff9fb0951790d6f3056ecc771ba1d60597278d57
MostofaSeum <mostofa.seum@brainicontech.com>
2026-07-08 16:01:45 +0600

    feat: implement core e-commerce models and configure admin dashboard functionality

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,4 @@
+from django.db.models import QuerySet
 from re import search
 from django.db.models import Count
 from django.contrib.admin import register
@@ -6,9 +7,51 @@ from django.utils.html import format_html
 from django.urls import reverse
 from . import models
 
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'inventory'
+    parameter_name = 'inventory'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('<10', 'Low')
+        ]
+
+    def queryset(self, request, queryset: QuerySet):
+        if self.value() == '<10':
+            return queryset.filter(inventory__lt=10)
+        return queryset
+
+
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price']
+    autocomplete_fields = ['collection']
+    prepopulated_fields = {
+        'slug': ['title']
+    }
+    actions = ['clear_inventory']
+    list_display = ['title', 'unit_price','inventory_status', 'collection_title']
+    list_editable = ['unit_price']
+    list_filter = ['collection', 'last_update', InventoryFilter]
+    list_per_page = 10
+    list_select_related = ['collection']
+    search_fields = ['title']
+
+    def collection_title(self, product):
+        return product.collection.title
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'Low'
+        return 'OK'
+
+    @admin.action(description='Clear inventory')
+    def clear_inventory(self, request, queryset):
+        updated_count = queryset.update(inventory=0)
+        self.message_user(
+            request,
+            f'{updated_count} products were successfully updated.'
+        )
     
 
 @admin.register(models.Customer)
@@ -16,6 +59,7 @@ class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'membership','order_list']
     list_editable = ['membership']
     ordering = ['first_name', 'last_name']
+    list_filter = ['membership']
     list_per_page = 10
     search_fields = ['first_name__istartswith','last_name__istartswith']
 
@@ -29,6 +73,7 @@ class CustomerAdmin(admin.ModelAdmin):
 
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['customer']
     list_display = ['id', 'placed_at', 'customer_name']
     ordering = ['-placed_at']
     list_per_page = 10
@@ -42,6 +87,7 @@ class CollectionAdmin(admin.ModelAdmin):
     list_display = ['title', 'Product_Count']
     ordering = ['title']
     list_per_page = 10
+    search_fields = ['title']
     
     def Product_Count(self,collection):
         return collection.Product_Count
