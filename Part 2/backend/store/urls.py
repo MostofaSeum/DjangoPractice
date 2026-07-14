ab78daec5a8b7b490b3345b4544e9882e9f7e92f
MostofaSeum <mostofa.seum@brainicontech.com>
2026-07-14 16:22:19 +0600

    feat: implement ProductViewSet and CollectionViewSet with restricted deletion logic in store app

--- Part 2/backend/store/urls.py
+++ Part 2/backend/store/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 from . import views
+
+router = DefaultRouter()
+router.register('products', views.ProductViewSet)
+router.register('collections', views.CollectionViewSet)
+urlpatterns = router.urls
+
 # URLConf
-urlpatterns = [
-    path('products/', views.ProductListCreateAPIView.as_view()),
-    path('products/<int:pk>/', views.ProductDetails.as_view()),
-    path('collections/', views.CollectionList.as_view()),
-    path('collections/<int:pk>/', views.CollectionDetail.as_view()),
-]
+# urlpatterns = [
+#     # path('products/', views.ProductListCreateAPIView.as_view()),
+#     # path('products/<int:pk>/', views.ProductDetails.as_view()),
+#     # path('collections/', views.CollectionList.as_view()),
+#     # path('collections/<int:pk>/', views.CollectionDetail.as_view()),
+# ]
--- Part 2/backend/store/views.py
+++ Part 2/backend/store/views.py
@@ -1,3 +1,4 @@
+from backend.store.models import OrderItem
 from django.http import request
 from store.serializers import CollectionSerializer, CollectionDetailSerializer
 from store.models import Collection
@@ -10,32 +11,29 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.mixins import ListModelMixin, CreateModelMixin
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework import ModelViewSet
+from rest_framework.viewsets import ModelViewSet
 from .models import Product
 from .serializers import ProductSerializers
 
 # Create your views here.
-class ProductListView(ModelViewSet):
+class ProductViewSet(ModelViewSet):
     queryset = Product.objects.select_related('collection').all()
     serializer_class = ProductSerializers
     def get_serializer_context(self):
         return {'request': self.request}
-
-    def delete(self, request, pk):
-        product = get_object_or_404(Product, pk=pk)
-        if product.orderitem_set.count() > 0:
-            return Response(status = status.HTTP_405_METHOD_NOT_ALLOWED)
-        product.delete()
-        return Response(status = status.HTTP_204_NO_CONTENT)
-
+    def destroy(self, request, *args, **kwargs):
+        if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
+            return Response({'error': 'Product cannot be deleted because it is associated with an order item.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        super().destroy(request, *args, **kwargs)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(product_count=Count('product')).all()
     serializer_class = CollectionSerializer
 
-    def delete(self, request, pk):
-        collection = get_object_or_404(Collection, pk=pk)
-        collection.delete()
-        return Response(status = status.HTTP_204_NO_CONTENT)
+    def destroy(self, request, *args, **kwargs):
+        if Product.objects.filter(collection_id=kwargs['pk']).count() > 0:
+            return Response({'error': 'Collection cannot be deleted because it includes one or more products.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return super().destroy(request, *args, **kwargs)
 
