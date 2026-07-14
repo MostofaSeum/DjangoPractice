a305d63c4bcecd9b5e8b759c2bfe2a2296f99168
MostofaSeum <mostofa.seum@brainicontech.com>
2026-07-14 14:21:51 +0600

    feat: implement product and collection CRUD views using DRF APIView and decorators

--- Part 2/backend/store/views.py
+++ Part 2/backend/store/views.py
@@ -8,6 +8,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.mixins import ListModelMixin, CreateModelMixin
+from rest_framework.generic import ListCreateAPIView
 from .models import Product
 from .serializers import ProductSerializers
 
