83cf96235b118ba7c7d2377748a6d6e03d4f6028
MostofaSeum <mostofa.seum@brainicontech.com>
2026-07-15 12:31:38 +0600

    feat: implement viewsets and nested routers for products, collections, and reviews

--- Part 2/backend/store/urls.py
+++ Part 2/backend/store/urls.py
@@ -7,7 +7,7 @@ router.register('products', views.ProductViewSet, basename='products')
 router.register('collections', views.CollectionViewSet)
 
 products_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
-products_router.register('reviews', views.ReviewViewSet, basename='products-reviews')
+products_router.register('reviews', views.ReviewViewSet, basename='product-reviews')
 
 urlpatterns = router.urls + products_router.urls
     
