e82090bb6319b580dbd65200b74fa24ce8045358
MostofaSeum <mostofa.seum@brainicontech.com>
2026-07-19 16:34:03 +0600

    feat: create DRF serializers for products, collections, reviews, and shopping cart models

--- backend/store/serializers.py
+++ backend/store/serializers.py
@@ -68,7 +68,11 @@ class AddCartItemSerializers(serializers.ModelSerializer):
     product_id = serializers.IntegerField()
     quantity = serializers.IntegerField(min_value=1, max_value=100)
 
-    
+
+    def validate_product_id(self, value):
+        if not Product.objects.filter(pk=value).exists():
+            raise serializers.ValidationError('No product with this ID exists.')
+        return value
 
     def save(self, **kwargs):
         product_id = self.validated_data['product_id']
