9734504b3582a1c60095f7dc00fc26dff445d3d1
MostofaSeum <mostofa.seum@brainicontech.com>
2026-07-20 14:51:55 +0600

    feat: implement custom user model, update product slug field, and register extended product admin

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,5 +1,6 @@
 from store.models import Product
 from django.contrib import admin
+
 from django.contrib.contenttypes.admin import GenericTabularInline
 from store.admin import ProductAdmin
 from tags.models import TaggedItem
