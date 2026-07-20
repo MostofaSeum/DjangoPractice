2ee20033c7cd294c1239dd7aff9d68474d4784fc
MostofaSeum <mostofa.seum@brainicontech.com>
2026-07-20 16:43:50 +0600

    feat: initialize storefront domain models including Product, Customer, Order, and Cart schemas

--- backend/store/models.py
+++ backend/store/models.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.conf import settings
 from django.db.models import Model
 from django.core.validators import MinValueValidator
@@ -60,14 +61,14 @@ class Customer(models.Model):
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name}'
 
+    @admin.display(ordering='user__first_name')
     def first_name(self):
         return self.user.first_name
 
+    @admin.display(ordering='user__last_name')
     def last_name(self):
         return self.user.last_name
 
-    def email(self):
-        return self.user.email
 
     class Meta:
         ordering = ['user__first_name', 'user__last_name']
