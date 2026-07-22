37adfe7221b61b96aa86a35702356752b6026902
MostofaSeum <mostofa.seum@brainicontech.com>
2026-07-22 14:17:15 +0600

    feat: configure custom user model with Djoser authentication and serializers

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,11 +1,15 @@
 from rest_framework import serializers
-from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer 
+from djoser.serializers import UserSerializer as BaseUserSerializer, UserCreateSerializer as BaseUserCreateSerializer 
 from .models import User
+from store.models import Customer
 
 
 class UserCreateSerializer(BaseUserCreateSerializer):
-
     class Meta(BaseUserCreateSerializer.Meta):
         model = User
         fields = ['id', 'username', 'email', 'password', 'first_name', 'last_name']
 
+class UserSerializer(BaseUserSerializer):
+    class Meta(BaseUserSerializer.Meta):
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
--- backend/storefront/settings.py
+++ backend/storefront/settings.py
@@ -187,8 +187,11 @@ AUTH_USER_MODEL = 'core.User'
 DJOSER = {
     "SERIALIZERS": {
         "user_create": "core.serializers.UserCreateSerializer",
+        "user": "core.serializers.UserSerializer",
+        "current_user": "core.serializers.UserSerializer",
     }
 }
+
 
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
