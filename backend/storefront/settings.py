8c605e197aa7389c2cf817b132bb5aa00b599ee0
MostofaSeum <mostofa.seum@brainicontech.com>
2026-07-26 09:53:02 +0600

    feat: initialize project settings with DRF, CORS, and custom user model configurations

--- backend/storefront/settings.py
+++ backend/storefront/settings.py
@@ -60,7 +60,6 @@ MIDDLEWARE = [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -161,7 +160,8 @@ REST_FRAMEWORK = {
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    "https://vibemart-flax.vercel.app",
 ]
 
 import os
