1ec2cbf3f6bda40c28431bf54d8673d6e8bd880e
MostofaSeum <mostofa.seum@brainicontech.com>
2026-07-29 10:33:08 +0600

    feat: implement email notification service in playground view

--- backend/playground/views.py
+++ backend/playground/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-from django.core.mail import send_mail,BadHeaderError
+from django.core.mail import send_mail,mail_admins,BadHeaderError
 
 
 def say_hello(request):
     try:
         send_mail('subject', 'message', 'mostofaseum8@gmail.com', ['mostofa.seum@brainicontech.com'])
-        print("EMAIL SENT SUCCESSFULLY!")
-    except Exception as e:
-        print(f"EMAIL ERROR: {e}")
+    except BadHeaderError:
+        pass
     return render(request, 'hello.html', {'name': 'Mosh'})
