42cd74f0301bdee51307aec0c87555f2f4f22a36
MostofaSeum <mostofa.seum@brainicontech.com>
2026-07-29 10:06:41 +0600

    feat: add say_hello view to handle email sending via Django mail utility

--- backend/playground/views.py
+++ backend/playground/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.core.mail import send_mail,BadHeaderError
 
 
 def say_hello(request):
+    try:
+        send_mail('subject', 'message', 'mostofaseum8@gmail.com', ['[mostofa.seum@brainicontech.com]'])
+    except BadHeaderError:
+        pass
     return render(request, 'hello.html', {'name': 'Mosh'})
