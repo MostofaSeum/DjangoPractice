8fbd15b3315e23444b9a67428610b0ae1b62b0fe
MostofaSeum <mostofa.seum@brainicontech.com>
2026-07-29 11:25:36 +0600

    feat: implement email notification system using BaseEmailMessage and custom templates

--- backend/playground/views.py
+++ backend/playground/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.core.mail import EmailMessage,BadHeaderError
-
+from templated_mail.mail import BaseEmailMessage
 
 def say_hello(request):
     try:
-        message = EmailMessage('subject', 'message', 'mostofaseum8@gmail.com', ['mostofa.seum@brainicontech.com'])
-        message.attach_file('D:/Brainicon Technology/storefront/backend/playground/static/images/Sea.png')
-
-        message.send()
+        # message = EmailMessage('subject', 'message', 'mostofaseum8@gmail.com', ['mostofa.seum@brainicontech.com'])
+        # message.attach_file('D:/Brainicon Technology/storefront/backend/playground/static/images/Sea.png')
+        message = BaseEmailMessage(
+            template_name='emails/hello.html',
+            context={'name': 'Seum'},
+        )
+        message.send(['mostofa.seum@brainicontech.com'])
     except BadHeaderError:
         pass
     return render(request, 'hello.html', {'name': 'Mosh'})
