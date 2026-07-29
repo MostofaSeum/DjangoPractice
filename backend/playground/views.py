5250e51fbac32bf120f1b4c6882fcfb678090f51
MostofaSeum <mostofa.seum@brainicontech.com>
2026-07-29 10:52:42 +0600

    feat: add say_hello view with email functionality to playground app

--- backend/playground/views.py
+++ backend/playground/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from django.core.mail import send_mail,mail_admins,BadHeaderError
+from django.core.mail import EmailMessage,BadHeaderError
 
 
 def say_hello(request):
     try:
-        send_mail('subject', 'message', 'mostofaseum8@gmail.com', ['mostofa.seum@brainicontech.com'])
+        message = EmailMessage('subject', 'message', 'mostofaseum8@gmail.com', ['mostofa.seum@brainicontech.com'])
+        
     except BadHeaderError:
         pass
     return render(request, 'hello.html', {'name': 'Mosh'})
