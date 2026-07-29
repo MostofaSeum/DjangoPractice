00b938c4d8d2497e6146bc20acb110b8461fc72f
MostofaSeum <mostofa.seum@brainicontech.com>
2026-07-29 10:18:58 +0600

    feat: initialize Django project settings with apps, CORS, and auth configurations

--- backend/storefront/settings.py
+++ backend/storefront/settings.py
@@ -205,3 +205,10 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
     "ACCESS_TOKEN_LIFETIME": timedelta(days=1)
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 2525
+EMAIL_USER = ''
+EMAIL_HOST_PASSWORD = ''
+DEFAULT_FROM_EMAIL = 'mostofaseum8@gmail.com'
