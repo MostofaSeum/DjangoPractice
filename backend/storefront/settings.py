22e293a4a4e2699c9c1fed7186fe021ed7f4fc3f
MostofaSeum <mostofa.seum@brainicontech.com>
2026-07-30 15:18:20 +0600

    feat: initialize Django project settings with REST framework, CORS, and email configurations

--- backend/storefront/settings.py
+++ backend/storefront/settings.py
@@ -215,8 +215,9 @@ SIMPLE_JWT = {
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'localhost'
-EMAIL_PORT = 2525
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-DEFAULT_FROM_EMAIL = 'mostofaseum8@gmail.com'
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'mostofaseum8@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'aqab auut ziry xtua')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'VibeMart <mostofaseum8@gmail.com>')
