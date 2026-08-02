d937fa5e93e7a262cfee7684a9f70aa6566f8e8c
MostofaSeum <mostofa.seum@brainicontech.com>
2026-08-02 12:33:26 +0600

    feat: initialize project settings with database, authentication, and CORS configurations

--- backend/storefront/settings.py
+++ backend/storefront/settings.py
@@ -84,13 +84,6 @@ INTERNAL_IPS = [
     '127.0.0.1',
     # ...
 ]
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://localhost:8001',
-    'http://127.0.0.1:8001',
-    'https://vibemart-flax.vercel.app',
-]
-CORS_ALLOW_ALL_ORIGINS = True
 ROOT_URLCONF = 'storefront.urls'
 
 TEMPLATES = [
@@ -245,4 +238,16 @@ else:
 ALLOWED_HOSTS = ['*']
 
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = False
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
