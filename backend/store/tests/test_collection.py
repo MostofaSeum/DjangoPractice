4c450e3974a9700f9330ab6a2b8eb1522e729b7b
MostofaSeum <mostofa.seum@brainicontech.com>
2026-08-04 16:46:07 +0600

    test: implement authentication fixtures and collection creation unit tests

--- backend/store/tests/test_collection.py
+++ backend/store/tests/test_collection.py
@@ -16,13 +16,15 @@ class TestCreateCollection:
         response = create_collection({'title': 'a'})
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-    def test_if_user_is_not_admin_return_403(self, api_client,create_collection):
-        api_client.force_authenticate(user={})
+    def test_if_user_is_not_admin_return_403(self, api_client,create_collection,authenticate):
+        authenticate()
+
         response = create_collection({'title': 'a'})
+        
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
-    def test_if_data_is_invalid_returns_400(self, api_client,create_collection):
-        api_client.force_authenticate(user=User(is_staff = True))
+    def test_if_data_is_invalid_returns_400(self, api_client,create_collection,authenticate):
+        authenticate(is_staff = True)
         response = create_collection({'title': ''})
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.data['title'] is not None
