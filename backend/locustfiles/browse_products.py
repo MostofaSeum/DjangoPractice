2c5af253fc588880c009f893165af869b58369e9
MostofaSeum <mostofa.seum@brainicontech.com>
2026-08-06 10:53:00 +0600

    feat: add locust load testing script for product browsing and cart operations

--- backend/locustfiles/browse_products.py
+++ backend/locustfiles/browse_products.py
@@ -5,12 +5,14 @@ class WebsiteUser(HttpUser):
     wait_time = between(1, 5)
     @task(2)
     def view_product(self):
+        print("View product")
         collection_id = randint(2,6)
         response = self.client.get(f"/store/products?collection_id={collection_id}",name="/store/products")
         response.raise_for_status()
 
     @task(4)
     def view_products(self):
+        print("View product details")
         product_id = randint(1,1000)
         response = self.client.get(f"/store/products/{product_id}",name="/store/products/:id")
         response.raise_for_status()
@@ -18,6 +20,7 @@ class WebsiteUser(HttpUser):
 
     @task(1)
     def add_to_cart(self):
+        print("Add to cart")
         product_id = randint(1,10)
         response = self.client.post(f"/store/carts/{self.cart_id}/items/",name="/store/carts/items", json = {"product_id": product_id,"quantity":1})
         response.raise_for_status()
