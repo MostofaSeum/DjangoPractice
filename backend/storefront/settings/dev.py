6cdb937dd54b0cd65e7fb40b640e29252ad5a51b
MostofaSeum <mostofa.seum@brainicontech.com>
2026-08-10 12:24:38 +0600

    feat: add development settings configuration and integrate .env file support into Docker services

--- backend/storefront/settings/dev.py
+++ backend/storefront/settings/dev.py
@@ -69,5 +69,15 @@ CACHES = {
     }
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-DEFAULT_FROM_EMAIL = 'VibeMart <noreply@vibemart.com>'
+# Email Settings
+if os.environ.get('EMAIL_HOST'):
+    EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
+    EMAIL_HOST = os.environ.get('EMAIL_HOST')
+    EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+    EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True').lower() in ['true', '1']
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+    DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'VibeMart <mostofaseum8@gmail.com>')
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'VibeMart <noreply@vibemart.com>'
