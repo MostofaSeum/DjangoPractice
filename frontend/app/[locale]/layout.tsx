a550db2610810a6dd0aa8434327964e3802d254b
MostofaSeum <mostofa.seum@brainicontech.com>
2026-08-30 13:28:45 +0600

    feat: initialize Next.js project with next-intl localization and localized routing support

--- frontend/app/[locale]/layout.tsx
+++ frontend/app/[locale]/layout.tsx
@@ -1,3 +1,6 @@
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+import { LanguageProvider } from "@/store/LanguageContext";
 import React from "react";
 
 export default async function LocaleLayout({
@@ -5,5 +8,11 @@ export default async function LocaleLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  const messages = await getMessages();
+
+  return (
+    <NextIntlClientProvider messages={messages}>
+      <LanguageProvider>{children}</LanguageProvider>
+    </NextIntlClientProvider>
+  );
 }
