2cce217ac1d199d32e3dd61a29f875da7d9876b8
MostofaSeum <mostofa.seum@brainicontech.com>
2026-09-09 10:23:31 +0600

    feat: initialize frontend next.config.ts and backend common.py configuration settings

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -23,6 +23,11 @@ const nextConfig: NextConfig = {
     minimumCacheTTL: 60,
     remotePatterns: [
       {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+      {
         protocol: "http",
         hostname: "127.0.0.1",
         port: "8000",
